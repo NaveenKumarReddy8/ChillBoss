26e09cbd44c6a96c44479e4e3c2ebe60a01cc5de
NaveenKumarReddy8 <mr.naveen8@gmail.com>
2020-10-02 16:52:45 +0530

    completed the internally required methods for pointer class in move.py

--- happyboss/move.py
+++ happyboss/move.py
@@ -8,9 +8,14 @@ from pyautogui import size, moveTo
 
 class Pointer:
     def __init__(
-        self, shape: str = "random", sleep_time: int = 30, motion_time: int = 0
+        self,
+        movement: str = "random",
+        length: int = 100,
+        sleep_time: int = 30,
+        motion_time: int = 0,
     ):
-        self._shape: str = shape
+        self._movement: str = movement
+        self._length: int = length
         self._sleep_time: int = sleep_time
         self._motion_time: int = motion_time
         self._x_pixels: int
@@ -24,12 +29,22 @@ class Pointer:
         random_y_pixel: int = randrange(start=0, stop=self._y_pixels)
         return random_x_pixel, random_y_pixel
 
-    def _get_coordinates(self) -> Tuple[Tuple[int, int]]:
-        shapes_allowed: dict = {
-            "random": self.randomise_movement,
-            "square": self.square_movement,
-            "triangle": self.triangle_movement,
-        }
+    def _get_square_coordinates(
+        self,
+    ) -> Tuple[Tuple[int, int], Tuple[int, int], Tuple[int, int], Tuple[int, int]]:
+        half_length: int = self._length // 2
+        return (
+            (self._x_center - half_length, self._y_center - half_length),
+            (self._x_center + half_length, self._y_center - half_length),
+            (self._x_center + half_length, self._y_center + half_length),
+            (self._x_center - half_length, self._y_center + half_length),
+        )
 
     def _move_pointer(self, x_coordinate: int, y_coordinate: int) -> None:
         moveTo(x=x_coordinate, y=y_coordinate, duration=self._motion_time)
+
+    def move_the_mouse_pointer(self) -> None:
+        try:
+            pass
+        except KeyboardInterrupt:
+            pass
