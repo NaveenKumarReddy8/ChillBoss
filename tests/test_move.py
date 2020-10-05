6e26f6121d5b87e500a236a66d64ccf307e57147
NaveenKumarReddy8 <mr.naveen8@gmail.com>
2020-10-05 12:08:39 +0530

    removed unused variables

--- tests/test_move.py
+++ tests/test_move.py
@@ -15,7 +15,7 @@ def pointer(mocker: MockerFixture) -> Pointer:
     return Pointer()
 
 
-def test_get_random_coordinates(pointer:Pointer) -> None:
+def test_get_random_coordinates(pointer: Pointer) -> None:
     random_x_coordinate: int
     random_y_coordinate: int
     random_x_coordinate, random_y_coordinate = pointer._get_random_coordinates()
@@ -25,13 +25,13 @@ def test_get_random_coordinates(pointer:Pointer) -> None:
 
 
 @pytest.mark.parametrize(
-    "x_center, y_center, length, result",
+    "x_pixels, y_pixels, length, result",
     [
-        (500, 300, 50, ((475, 275), (525, 275), (525, 325), (475, 325))),
-        (1000, 1500, 100, ((950, 1450), (1050, 1450), (1050, 1550), (950, 1550))),
+        (1000, 600, 50, ((475, 275), (525, 275), (525, 325), (475, 325))),
+        (2000, 3000, 100, ((950, 1450), (1050, 1450), (1050, 1550), (950, 1550))),
         (
-            SCREEN_WIDTH // 2,
-            SCREEN_LENGTH // 2,
+            SCREEN_WIDTH,
+            SCREEN_LENGTH,
             DEFAULT_LENGTH,
             (
                 (
@@ -54,9 +54,11 @@ def test_get_random_coordinates(pointer:Pointer) -> None:
         ),
     ],
 )
-def test_get_square_coordinates(x_center: int, y_center: int, length: int, result, pointer) -> None:
-    pointer._x_center: int = x_center
-    pointer._y_center: int = y_center
+def test_get_square_coordinates(
+    x_pixels: int, y_pixels: int, length: int, result, pointer
+) -> None:
+    pointer._x_pixels: int = x_pixels
+    pointer._y_pixels: int = y_pixels
     pointer._length: int = length
     corners = pointer._get_square_coordinates()
     assert corners[0] == result[0]
@@ -65,10 +67,17 @@ def test_get_square_coordinates(x_center: int, y_center: int, length: int, resul
     assert corners[3] == result[3]
 
 
-def test_move_pointer(mocker, pointer) -> None:
-    mocked_moveTo = mocker.patch("happyboss.move.moveTo")
-    pointer._move_pointer(mocker.Mock(), mocker.Mock())
-    mocked_moveTo.assert_called_once()
+@pytest.mark.parametrize(
+    "x_pixels, y_pixels, length", [(300, 200, 600), (500, 150, 150)]
+)
+def test_get_square_coordinates_valueerror_out_of_bounds(
+    x_pixels: int, y_pixels: int, length: int, pointer
+) -> None:
+    with pytest.raises(ValueError):
+        pointer._x_pixels: int = x_pixels
+        pointer._y_pixels: int = y_pixels
+        pointer._length: int = length
+        pointer._get_square_coordinates()
 
 
 def test_random_movement(mocker, pointer) -> None:
@@ -76,16 +85,16 @@ def test_random_movement(mocker, pointer) -> None:
         "happyboss.move.Pointer._get_random_coordinates",
         return_value=(mocker.Mock(), mocker.Mock()),
     )
-    side_effects_by_move_pointer = [None, None, None, None, KeyboardInterrupt]
-    mocked_move_pointer = mocker.patch(
-        "happyboss.move.Pointer._move_pointer",
-        side_effect=side_effects_by_move_pointer,
+    side_effects_by_moveTo = [None, None, None, None, KeyboardInterrupt]
+    mocked_moveTo = mocker.patch(
+        "happyboss.move.moveTo",
+        side_effect=side_effects_by_moveTo,
     )
     mocker.patch("happyboss.move.sleep")
 
     pointer._random_movement()
 
-    assert mocked_move_pointer.call_count == len(side_effects_by_move_pointer)
+    assert mocked_moveTo.call_count == len(side_effects_by_moveTo)
 
 
 def test_squared_movement(mocker, pointer) -> None:
@@ -93,16 +102,16 @@ def test_squared_movement(mocker, pointer) -> None:
         "happyboss.move.Pointer._get_square_coordinates",
         return_value=((mocker.Mock(), mocker.Mock()) for i in range(4)),
     )
-    side_effects_by_move_pointer = [None, None, None, KeyboardInterrupt]
-    mocked_move_pointer = mocker.patch(
-        "happyboss.move.Pointer._move_pointer",
-        side_effect=side_effects_by_move_pointer,
+    side_effects_by_moveTo = [None, None, None, KeyboardInterrupt]
+    mocked_moveTo = mocker.patch(
+        "happyboss.move.moveTo",
+        side_effect=side_effects_by_moveTo,
     )
     mocker.patch("happyboss.move.sleep")
 
     pointer._squared_movement()
 
-    assert mocked_move_pointer.call_count == len(side_effects_by_move_pointer)
+    assert mocked_moveTo.call_count == len(side_effects_by_moveTo)
 
 
 @pytest.mark.parametrize(
@@ -116,7 +125,7 @@ def test_move_the_mouse_pointer(movement, movement_method, mocker, pointer) -> N
     mocked_movement_method.assert_called_once()
 
 
-def test_move_the_mouse_pointer_for_new_kind_of_movement(mocker, pointer) -> None:
+def test_move_the_mouse_pointer_for_new_kind_of_movement(pointer) -> None:
     with pytest.raises(KeyError):
         pointer._movement = "triangle"
         pointer.move_the_mouse_pointer()
