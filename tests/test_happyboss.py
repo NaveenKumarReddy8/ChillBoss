1a5c81b6419c7d803068d9169bd4ada15f0d17d8
NaveenKumarReddy8 <mr.naveen8@gmail.com>
2020-10-14 09:56:45 +0530

    completed cli

--- tests/test_happyboss.py
+++ tests/test_happyboss.py
@@ -1,5 +1,6 @@
-from happyboss import __version__
-
-
 def test_version():
+    # Importing happyboss inside the test function to register the bogus
+    # pyautogui class before actual pyautogui module gets loaded to sys modules.
+    from happyboss import __version__
+
     assert __version__ == "0.1.0"
