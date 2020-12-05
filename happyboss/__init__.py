fae0c36b2f3b2b81c7ae229c0bc2329730aa8224
NaveenKumarReddy8 <mr.naveen8@gmail.com>
2020-12-05 08:26:10 +0530

    Cleanup for release

--- happyboss/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Happyboss package."""
-
-from happyboss.move import Pointer
-
-__version__ = "0.1.0"
-
-__all__ = ["Pointer"]
--- tests/test_happyboss.py
+++ tests/test_happyboss.py
@@ -1,6 +1,6 @@
 def test_version():
-    # Importing happyboss inside the test function to register the bogus
+    # Importing chillboss inside the test function to register the bogus
     # pyautogui class before actual pyautogui module gets loaded to sys modules.
-    from happyboss import __version__
+    from chillboss import __version__
 
     assert __version__ == "0.1.0"
